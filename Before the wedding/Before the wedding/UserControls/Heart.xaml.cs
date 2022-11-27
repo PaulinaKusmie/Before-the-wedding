3ff05fb0087597f19395b25fe6219c5326a2eb57
Paulina <59999364+PaulinaKusmie@users.noreply.github.com>
2022-11-27 01:20:32 +0100

    New Components!!

---
+++ Before the wedding/Before the wedding/UserControls/Heart.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Shapes;
+using Xamarin.Forms.Xaml;
+
+namespace Before_the_wedding
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Heart : ContentView
+    {
+        public Heart()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty TextProperty =
+            BindableProperty.Create("Text", typeof(string), typeof(Heart), string.Empty,
+                defaultBindingMode: BindingMode.TwoWay,
+                propertyChanged: textPropertyChanged);
+
+        private static void textPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (Heart)bindable;
+            (control.FindByName("textLabel") as Label).Text = newValue.ToString();
+        }
+
+
+        public string Text
+        {
+            get { return base.GetValue(TextProperty).ToString(); }
+            set
+            {
+                base.SetValue(TextProperty, value);
+            }
+        }
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            path.RenderTransform = new ScaleTransform() { ScaleX = 0.5, ScaleY = 0.5 };
+        }
+    }
+}
